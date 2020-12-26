b31a9c6a4760d7459d66143f9e27d5d1dfbce5cf
EveryGameSPlay <repsbro@gmail.com>
2020-12-26 13:46:57 +0300

    Camera height fit

--- Assets/_ProjectFiles/Scripts/AspectFit.cs
+++ Assets/_ProjectFiles/Scripts/AspectFit.cs
@@ -7,7 +7,15 @@ using UnityEngine;
 
 public class AspectFit : MonoBehaviour
 {
+    public enum FitMode
+    {
+        Width,
+        Height
+    }
     [SerializeField] private float widthFit;
+    [SerializeField] private float heightFit;
+    public FitMode fitMode;
+    
     
     // Start is called before the first frame update
     private void Start()
@@ -19,23 +27,38 @@ public class AspectFit : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         var widthHalf = widthFit / 2;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.right*widthHalf);
-        Gizmos.DrawLine(transform.position, transform.position - Vector3.right*widthHalf);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * widthHalf);
+        Gizmos.DrawLine(transform.position, transform.position - Vector3.right * widthHalf);
+        
+        Gizmos.color = Color.blue;
+        var heightHalf = heightFit / 2;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * heightHalf);
+        Gizmos.DrawLine(transform.position, transform.position - Vector3.up * heightHalf);
     }
 
     [Button("Change screen")]
     private void ChangeScreen()
     {
-        Camera.main.orthographicSize = ScreenFitter.WidthOrtoPortrait(widthFit, Camera.main);
+        switch (fitMode)
+        {
+            case FitMode.Width:
+                Camera.main.orthographicSize = ScreenFitter.WidthOrtoPortrait(widthFit, Camera.main);
+                break;
+            case FitMode.Height:
+                Camera.main.orthographicSize = ScreenFitter.HeightOrtoPortrait(heightFit);
+                break;
+        }
     }
 }
 
 public static class ScreenFitter
 {
+    // WIDTH
+    // Width = height * aspectRatio
     // Portrait
     public static float WidthOrtoPortrait(float width, Camera camera)
     {    
-        Vector2 screen = new Vector2((float)camera.pixelHeight,(float)camera.pixelWidth);
+        Vector2 screen = new Vector2((float)camera.pixelWidth,(float)camera.pixelHeight);
 
         return WidthOrtoPortrait(width, screen);
     }
@@ -56,8 +79,8 @@ public static class ScreenFitter
     {
         var aspectRatio = 0f;
 
-        if (screen.y != 0)
-            aspectRatio = screen.x / screen.y;
+        if (screen.x!= 0)
+            aspectRatio = screen.y / screen.x;
 
         return WidthOrtoPortrait(width, aspectRatio);
     }
@@ -66,5 +89,13 @@ public static class ScreenFitter
     public static float WidthOrtoPortrait(float width, float aspectRatio)
     {
         return width * aspectRatio * 0.5f;
+    }
+
+    
+    // HEIGHT
+    // Height = 2f * orhtoSize
+    public static float HeightOrtoPortrait(float height)
+    {
+        return height/2;
     }
 }
